69e88e56a60a680ff63f0cf3877b4f904a90b35e
Jakub Cegłowski <jcplewiska@interia.pl>
2025-12-21 15:59:47 +0100

    Restore files

---
+++ src/main/java/pl/wsb/fitnesstracker/user/internal/UserController.java
@@ -0,0 +1,35 @@
+package pl.wsb.fitnesstracker.user.internal;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.wsb.fitnesstracker.user.api.UserDto;
+
+import java.util.List;
+
+/**
+ * UserController is responsible for handling HTTP requests related to user operations.
+ * It provides endpoints for retrieving and creating users.
+ */
+@RestController
+@RequestMapping("/v1/users")
+class UserController {
+
+    private final UserServiceImpl userService;
+
+    private final UserMapper userMapper;
+
+    public UserController(UserServiceImpl userService, UserMapper userMapper) {
+        this.userService = userService;
+        this.userMapper = userMapper;
+    }
+
+    @GetMapping
+    public List<UserDto> getAllUsers() {
+        return userService.findAllUsers()
+                .stream()
+                .map(userMapper::toDto)
+                .toList();
+    }
+}
+
